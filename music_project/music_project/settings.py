a0e0b5b49bdf50e577e4956c6494cd3122fad972
v4filippoff <v4filippoff@yandex.ru>
2022-01-14 01:06:10 +0300

    Changed database sqlite -> postgresql

--- music_project/music_project/settings.py
+++ music_project/music_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'music_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'music_api',
+        'USER': 'django_user',
+        'PASSWORD': 'django_user',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
