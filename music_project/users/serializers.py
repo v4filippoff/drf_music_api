4b62f550e393f557155ecc1b1b39630267112e35
v4filippoff <v4filippoff@yandex.ru>
2022-01-20 00:08:50 +0300

    Create UserSerializer and write a test for it

---
+++ music_project/users/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'description',
+            'country',
+            'avatar'
+        ]
---
+++ music_project/users/tests/test_serializers.py
@@ -0,0 +1,49 @@
+from io import BytesIO
+
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create(
+            username='TestUser',
+            description='Test Description',
+            country='Russia'
+        )
+        self.user2 = User.objects.create(
+            username='TestUser2',
+            description='Test Description2',
+            country='USA'
+        )
+
+        image = BytesIO()
+        Image.new('RGB', (100, 100)).save(image, 'JPEG')
+        image.seek(0)
+        self.user1.avatar = SimpleUploadedFile('image.jpg', image.getvalue())
+        self.user1.save()
+
+    def test_all_fields(self):
+        data = UserSerializer([self.user1, self.user2], many=True).data
+        expected_data = [
+            {
+                'id': self.user1.id,
+                'username': 'TestUser',
+                'description': 'Test Description',
+                'country': 'Russia',
+                'avatar': self.user1.avatar.url
+            },
+            {
+                'id': self.user2.id,
+                'username': 'TestUser2',
+                'description': 'Test Description2',
+                'country': 'USA',
+                'avatar': None
+            }
+        ]
+        self.assertEqual(data, expected_data)
