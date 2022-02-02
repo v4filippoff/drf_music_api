c8f372b865850e49f36fed76bf0a1c4ad45640ba
v4filippoff <v4filippoff@yandex.ru>
2022-02-03 01:01:02 +0300

    Override UserAdmin class for adding custom user fields

--- music_project/users/admin.py
+++ music_project/users/admin.py
@@ -1,18 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
 from users.models import User, SocialLink, Follow
 
 
+# Форма для переопределения дефолтной формы юзера в админке
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+# Переопределяем дефолтный класс админки юзера, указывая кастомную форму (см. выше) и
+# дополнительные поля из нашего кастомного юзера
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'username',
-        'email',
-        'description',
-        'country',
-        'date_joined',
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': (
+                'description',
+                'country',
+                'avatar',
+            )}),
     )
-    list_display_links = ('username',)
 
 
 @admin.register(SocialLink)
