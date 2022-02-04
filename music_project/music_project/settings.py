92cbef679e2cda7761a6db293ccabc0ca8b9fbe2
v4filippoff <v4filippoff@yandex.ru>
2022-02-04 22:48:17 +0300

    Add global pagination in limit/offset style

--- music_project/music_project/settings.py
+++ music_project/music_project/settings.py
@@ -144,5 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
